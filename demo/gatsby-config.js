c59858263ae19227cda14c9e227cbf6a92c56252
Horacio Herrera <me@hherrerag.com>
2019-07-23 16:55:59 +0200

    add google analytics

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,17 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Theme Instagram Demo'
+    title: "Gatsby Theme Instagram Demo",
   },
-  plugins: [`@horacioh/gatsby-theme-mdx`, {
-    resolve: `@horacioh/gatsby-theme-instagram`,
-    options: {
-      username: 'doraforscale'
-    }
-  }, {
-    resolve: `@horacioh/gatsby-theme-instagram`,
-    options: {
-      type: 'hashtag',
-      hashtag: `snowing`,
-    }
-  }],
+  plugins: [
+    `@horacioh/gatsby-theme-mdx`,
+    {
+      resolve: `@horacioh/gatsby-theme-instagram`,
+      options: {
+        username: "doraforscale",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-87410585-4",
+      },
+    },
+  ],
 }
